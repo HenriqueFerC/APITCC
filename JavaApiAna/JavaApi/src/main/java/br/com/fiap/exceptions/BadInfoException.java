8c822a9cf0fc640a46561cb0e8f1451e155c9ba6
HenriqueFerC <129521477+HenriqueFerC@users.noreply.github.com>
2024-10-01 22:15:46 -0300

    Delete JavaApiAna directory

--- JavaApiAna/JavaApi/src/main/java/br/com/fiap/exceptions/BadInfoException.java
+++
@@ -1,8 +0,0 @@
-package br.com.fiap.exceptions;
-
-
-public class BadInfoException extends Exception {
-	public BadInfoException(String msg) {
-		super(msg);
-	}
-}
--- JavaApiAna/JavaApi/src/main/java/br/com/fiap/exceptions/IdNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.exceptions;
-
-public class IdNotFoundException extends Exception {
-	public IdNotFoundException(String msg) {
-		super(msg);
-	}
-}
